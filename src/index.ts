77d414ba5130dcdd710ab484d819303f769c3134
gabriel.vicente@lojasedmil.com.br <gabriel.vicente@lojasedmil.com.br>
2024-05-27 17:33:02 -0300

    first

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express from 'express'
+import swagger from 'swagger-ui-express'
+import json from './swagger.json'
+
+const app = express()
+
+app.use(express.json())
+app.use('/swagger', swagger.serve, swagger.setup(json))
+app.listen(80, ()=> console.log('running port 8090'))
